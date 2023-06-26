c6092e11339f15f12778f050b3ff75eb97224a22
Dan Berezin <Bogdanlinee@gmail.com>
2023-06-26 21:10:24 +0300

    Added file validation

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,15 +2,13 @@ const Product = require('../models/Product');
 const { StatusCodes } = require('http-status-codes');
 
 const createProduct = async (req, res) => {
-  console.log(req.body);
   const product = await Product.create(req.body);
   res.status(201).json({ product });
 }
 
 const getAllProducts = async (req, res) => {
   const products = await Product.find({});
-  console.log(products)
-  res.status(StatusCodes.OK).json({ products })
+  res.status(StatusCodes.OK).json({ products });
 }
 
 module.exports = { createProduct, getAllProducts };
--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,8 +1,24 @@
 const { StatusCodes } = require('http-status-codes');
 const path = require('path');
+const CustomError = require('../errors/index');
 
 const uploadProductImage = async (req, res) => {
+  const imageMaxSize = 1024 * 1024;
+
+  if (!req.files) {
+    throw new CustomError.BadRequestError('No file uploaded');
+  }
+
   const productImage = req.files.image;
+
+  if (!productImage.mimetype.statsWith('image/jpeg')) {
+    throw new CustomError.BadRequestError('Please upload image.');
+  }
+
+  if (productImage.size > imageMaxSize) {
+    throw new CustomError.BadRequestError('Image has to be less then ' + imageMaxSize);
+  }
+
   const imagePath = path.join(__dirname, '../public/uploads/' + `${productImage.name}`);
   await productImage.mv(imagePath);
   res.status(StatusCodes.OK).json({
