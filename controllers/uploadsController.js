1b4e8b416b7674a5378fe844223088b6efb5cfa2
Dan Berezin <Bogdanlinee@gmail.com>
2023-06-27 20:35:23 +0300

    Added cloudinary

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,8 +1,11 @@
 const { StatusCodes } = require('http-status-codes');
 const path = require('path');
 const CustomError = require('../errors/index');
+const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
+const { json } = require('express');
 
-const uploadProductImage = async (req, res) => {
+const uploadProductImageLocal = async (req, res) => {
   const imageMaxSize = 1024 * 1024;
 
   if (!req.files) {
@@ -24,6 +27,20 @@ const uploadProductImage = async (req, res) => {
   res.status(StatusCodes.OK).json({
     image: {
       src: `/uploads/${productImage.name}`
+    }
+  });
+}
+
+const uploadProductImage = async (req, res) => {
+  const result = await cloudinary.uploader.upload(req.files.image.tempFilePath, {
+    use_filename: true, folder: 'upload-test'
+  });
+
+  fs.unlinkSync(req.files.image.tempFilePath);
+
+  res.status(StatusCodes.OK).json({
+    image: {
+      src: result.secure_url
     }
   });
 }
